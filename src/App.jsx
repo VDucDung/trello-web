aa4c16e4ad68096ec3ca704ba8ea04cdd411a6b0
VDucDung <dunga3qv2@gmail.com>
2024-04-17 15:56:16 +0700

    ✨ Implement dark mode and light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import Button from '@mui/material/Button'
 import DeleteIcon from '@mui/icons-material/Delete'
 import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 function App() {
   return (
     <>
+      <ModeToggle />
       <div>vuducdung</div>
       <Typography variant="h1" color="text.secondary">Hello</Typography>
       <Button variant="text">Text</Button>
