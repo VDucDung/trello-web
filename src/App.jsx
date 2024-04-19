6a91dd6ae80212019e00024a3bd3ffd15c6dd88e
VDucDung <dunga3qv2@gmail.com>
2024-04-19 10:24:18 +0700

    🔧 Config absolute relative import

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { Route, Routes } from 'react-router-dom'
-import Board from './pages/Boards/_id'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Board from '~/pages/Boards/_id'
 function App() {
   return (
-    <>
-      <Routes>
-        <Route exact path="/Boards" element={<Board />} />
-      </Routes>
-    </>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/Boards" element={<Board />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
